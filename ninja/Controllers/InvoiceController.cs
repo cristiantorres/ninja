bf0fb798514f41e19c060589af79ac7bbd782c88
Cristian Torres <cristian.torres@ms.arba.gov.ar>
2019-05-14 16:49:29 -0300

    requerimiento

---
+++ ninja/Controllers/InvoiceController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ninja.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Invoice/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Invoice/Create
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Invoice/Create
+        [HttpPost]
+        public ActionResult New(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Invoice/Edit/5
+        public ActionResult Update(int id)
+        {
+            return View();
+        }
+
+        // POST: Invoice/Edit/5
+        [HttpPost]
+        public ActionResult Update(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Invoice/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Invoice/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
