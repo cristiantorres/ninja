09f68db7ab0fd4e4ddf844c2e8e34a588ae8f495
CRISTIAN FLAVIO TORRES <cristianlp.torr@gmail.com>
2019-05-18 13:01:20 -0300

    mejoras -- add Vieewmodels

---
+++ ninja/Models/InvoiceViewModels.cs
@@ -0,0 +1,18 @@
+﻿using ninja.Models.validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ninja.Models
+{
+    public class InvoiceViewModels
+    {
+        [Required]
+        public long Id { get; set; }
+
+        [Required,CustomTypeValidator]
+        public string Type { get; set; }
+    }
+}
