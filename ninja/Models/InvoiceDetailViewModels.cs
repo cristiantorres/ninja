afc7d1a385fafd79dc561e9d00905a891b5c1d54
Cristian Torres <cristian.torres@ms.arba.gov.ar>
2019-05-28 16:46:59 -0300

    ---

--- ninja/Models/InvoiceDetailViewModels.cs
+++ ninja/Models/InvoiceDetailViewModels.cs
@@ -12,23 +12,28 @@ namespace ninja.Models
         public double Taxes { get { return 1.21; } }
 
         [Required]
+        [System.ComponentModel.DisplayName("Invoice")]
         public long InvoiceId { get; set; }
 
         public long Id { get; set; }
 
         [Required]
+        [System.ComponentModel.DisplayName("Description")]
         public string Description { get; set; }
 
         [Required]
         public double Amount { get; set; }
 
         [Required]
+        [System.ComponentModel.DisplayName("Unit Price")]
         public double UnitPrice { get; set; }
 
         [Required]
+        [System.ComponentModel.DisplayName("Total Price")]
         public double TotalPrice { get; set; }
 
         [Required]
+        [System.ComponentModel.DisplayName("Total Price (With Taxes)")]
         public double TotalPriceWithTaxes { get; set; }
     }
 }
